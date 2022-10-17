0ea4b34feb0f5da8b2169cd1a9443efca9d3ba48
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-17 16:59:05 +0300

    One-use buttons

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
 
 */
 
-function logger() {
-    console.log('BUTTON CLICKED')
+function logger(event) {
+   console.log(event.target);
+   const currentBtn = event.target;
+   currentBtn.disabled = true;
+   currentBtn.removeEventListener('click', logger);
 }
 
 const btnCollection = document.querySelectorAll('button');
