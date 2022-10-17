80d7bb307aebc754372e8f034c6ed458d5d2f49a
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-17 16:16:57 +0300

    DOM welcome

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 
-
-function makeCounter() {
-    let i = 0;
-    return {
-        increment: function() {
-            return ++i;
-        },
-        decrement: () => --i
-    }
-    
-}
-
-
-/*
-Написати функцію createAdder, повертає іншу функцію. Повернена функція має прийняти число в якості аргументу і скласти його з аргументом createAdder
-
-createAdder(n)
-
-
-*/
-
-
-// function createAdder(n) {
-//     return function(m){   
-//         return n = n+m
-//     }
+// function click() {
+//     alert('Achievement unlocked')
 // }
 
-const createAdder = (n) => (m) => n+=m; 
 
-createAdder(5)(10) //виклик функції, що повертається з попередньої функції
+const btnCollection = document.querySelectorAll('button');
+
+for (const btn of btnCollection) {
+    btn.innerText = 'DON`T CLICK';
+}
