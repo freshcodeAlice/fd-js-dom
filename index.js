5cd6d5a9ad14ea246f8b78794661890b0cff02e8
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-17 14:44:01 +0300

    createAdder refactor

--- index.js
+++ index.js
@@ -6,10 +6,27 @@ function makeCounter() {
         increment: function() {
             return ++i;
         },
-        decrement: function(){
-            return --i;
-        }
+        decrement: () => --i
     }
     
 }
 
+
+/*
+Написати функцію createAdder, повертає іншу функцію. Повернена функція має прийняти число в якості аргументу і скласти його з аргументом createAdder
+
+createAdder(n)
+
+
+*/
+
+
+// function createAdder(n) {
+//     return function(m){   
+//         return n = n+m
+//     }
+// }
+
+const createAdder = (n) => (m) => n+=m; 
+
+createAdder(5)(10) //виклик функції, що повертається з попередньої функції
