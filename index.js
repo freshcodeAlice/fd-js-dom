2f801ed6605a0a2386733eec3844a5e832ca977b
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-18 14:22:43 +0300

    dispatchEvent

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-/*
-Використовуючи наявну колекцію з 5 кнопок, за натиснення на будь-яку кнопку виведіть на консоль "Клік по кнопці"
+document.addEventListener('keydown', handler);
+document.addEventListener('click', handler);
 
-*/
 
-function logger(event) {
-   console.log(event.target);
-   const currentBtn = event.target;
-   currentBtn.disabled = true;
-   currentBtn.removeEventListener('click', logger);
+
+function handler(event) {
+    console.log(event);
 }
 
-const btnCollection = document.querySelectorAll('button');
 
-for (const btn of btnCollection) {
-    btn.addEventListener('click', logger)
-}
 
+const ev = new PointerEvent('click');
+document.dispatchEvent(ev);
