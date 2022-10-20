fab0a6c5f51bc2870335b11eb194748ab5ce11c1
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-20 17:02:22 +0300

    Currency converter v2

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const form = document.querySelector('form');
 const resultField = document.querySelector('#resultField');
-const KOEFFICIENT = 36.6;
+const KOEFFICIENT = {
+    USD: 36.6,
+    EUR: 35.76,
+    PLN: 7.47,
+    UAH: 1
+}
 
 form.addEventListener('submit', submitHandler);
 
 function submitHandler (event) {
     event.preventDefault();
-    const {target: {amount: {value}}} = event;
-    if(Number.isNaN(Number(value))) {
+    const {target: {amount: {value: amount}, 
+            currencyInput: {value: currencyInput}, 
+            currencyOutput: {value: currencyOutput}}} = event;
+    if(Number.isNaN(Number(amount))) {
         resultField.textContent = 'Amount must be a number';
     } else {
-        resultField.textContent = value * KOEFFICIENT;
+        const res = amount * KOEFFICIENT[currencyInput] / KOEFFICIENT[currencyOutput]
+        resultField.textContent = res.toFixed(2);
     }
 }
 
