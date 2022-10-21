d6150bb080241ad4e3a1522e08887edd7f334660
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-21 15:38:08 +0300

    Dynamic creating

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
-const form = document.querySelector('form');
-const resultField = document.querySelector('#resultField');
-const KOEFFICIENT = {
-    USD: 36.6,
-    EUR: 35.76,
-    PLN: 7.47,
-    UAH: 1
-}
-
-form.addEventListener('submit', submitHandler);
-
-function submitHandler (event) {
-    event.preventDefault();
-    const {target: {amount: {value: amount}, 
-            currencyInput: {value: currencyInput}, 
-            currencyOutput: {value: currencyOutput}}} = event;
-    if(Number.isNaN(Number(amount))) {
-        resultField.textContent = 'Amount must be a number';
-    } else {
-        const res = amount * KOEFFICIENT[currencyInput] / KOEFFICIENT[currencyOutput]
-        resultField.textContent = res.toFixed(2);
-    }
-}
-
-
-
 /*
-Маємо input та article.
-Зробити конвертер валют.
-Користувач вводить в input число в долларах, при відправці форми в article вивести конвертовану суму в грн.
+Динамічне створення елементу
+
+1. Створити об'єкт елемента і отримати адресу цього об'єкта в пам'яті
+2. Доєднати елемент до батьківського (приаппендити)
 
 */
+
+const element = document.createElement('h1');
+element.textContent = 'Hello, DOM';
+document.body.append(element);
