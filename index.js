e58c493d9fd09591623005e15cb48d9515ceddee
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-31 14:35:17 +0200

    vacation task start resolving

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-console.log(userData);
-
-
 /* TODO:
 
 Частина 1:
@@ -17,3 +14,33 @@ https://pbs.twimg.com/media/EIopEQ6XYAEqzUk.jpg
 Тобто одночасно може бути обрана тільки одна картка.
 
 */
+
+const root = document.querySelector('#root');
+
+const cardArray = userData.map(user => createUserCard(user));
+
+function createUserCard(user){
+  
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('card-wrapper');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', user.profilePicture);
+    img.setAttribute('alt', user.name);
+    img.classList.add('avatar');
+
+    const h2 = document.createElement('h2');
+    h2.append(user.name);
+
+    const p = document.createElement('p');
+    p.classList.add('desc');
+    p.append(user.description);
+
+    const button = document.createElement('button');
+    button.append('Connent');
+
+    wrapper.append(img, h2, p, button);
+    return wrapper;
+}
+
+root.append(...cardArray);
--- userData.js
+++ userData.js
@@ -32,7 +32,7 @@ const userData = [
     {
         "id": 6,
         "name": "Nathan",
-        "description": "lkheklerhtlk3jtk34jh5kjhkwekfjekjfahe",
+        "description": "lkheklerh dsdfsdfdfsdfsdfdsfdsfdftl k3jtk34jh5kjhkw ekfjekjfahe",
         "profilePicture": "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
     },
     {
